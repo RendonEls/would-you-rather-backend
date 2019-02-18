045613e66b4f1cbec8cb7618ebffef11c3848e14
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-18 15:13:09 -0500

    seed data now populating to DB

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,35 +1,17 @@
 const mongoose = require('./connection')
 const { Question } = require('../models/Question')
-
-// const Question = mongoose.model('Question')
 const data = require('./seed-data.json')
 
 mongoose.Promise = Promise;
 
-Question.collection.insert(data).then(seededData => {
-    console.log(seededData)
-    mongoose.connection.close()
-}).catch(err => {
-    console.log(err)
-})
 
-// Question.remove({})
-//   .then(() => {
-//     Question.collection.insert(data)
-//       .then((data) => {
-//         console.log(data)
-//         process.exit()
-//       })
-//   })
-//   .catch((err) => {
-//     console.log(err)
-//   })
-// Question.find({}).remove({}).then(() => {
-//         Question.collection.insert(data).then(effect => {
-//             console.log(effect)
-//             process.exit()
-//         })
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
+Question.find({})
+.remove({}).then(() => {
+        Question.collection.insert(data).then(effect => {
+            console.log(effect)
+            process.exit()
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
