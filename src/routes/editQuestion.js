7071c28c64da5388edae88b637e190f108767024
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-18 15:37:36 -0500

    added render function to pages that will need the data for now

--- src/routes/editQuestion.js
+++ src/routes/editQuestion.js
@@ -1,10 +1,12 @@
 const express = require ('express')
 const router = express.Router()
+const {Question} = require('../models/Question')
 
 console.log('edit question routes ')
 
 router.get("/", (req, res) =>{
-    res.send("This is a edit question page")
+    Question.find().then(show => res.json(show))
+    // res.send("This is a edit question page")
 })
 router.put("/", (req, res) =>{
     res.send("This is PUT question route")
--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,10 +1,12 @@
 const express = require ('express')
 const router = express.Router()
+const {Question} = require('../models/Question')
 
 console.log('stats route ')
 
 router.get("/", (req, res) =>{
-    res.send("hi from Stats page")
+    Question.find().then(show => res.json(show))
+    // res.send("hi from Stats page")
 })
 
 module.exports = router
