302340d694562d7fed50ae59cbb978aed7e4e17c
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-18 15:29:18 -0500

    play renders data from seed file

--- src/routes/play.js
+++ src/routes/play.js
@@ -1,10 +1,12 @@
 const express = require ('express')
 const router = express.Router()
+const {Question} = require('../models/Question')
 
 console.log('play page ')
 
 router.get("/", (req, res) =>{
-    res.send("Hello from play page")
+    Question.find().then(show => res.json(show))
+    // res.send("Hello from play page")
 })
 router.post("/", (req, res) =>{
     res.send("POST ON PLAY page")
