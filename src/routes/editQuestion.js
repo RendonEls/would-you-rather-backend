4193c1a61aa69e5cc9c145498b90f1855908dee6
Pierson Brannan <plbrannan95@gmail.com>
2019-02-21 14:56:25 -0500

    Jimmy help, update button dynamically works

--- src/routes/editQuestion.js
+++ src/routes/editQuestion.js
@@ -13,24 +13,27 @@ router.put("/", (req, res) =>{
     // res.send("This is PUT question route")
     console.log("PUT request sent")
     // console.log(req.body._id)
-    console.log(req.body)
+    // console.log(req.body.question1)
+    console.log(req.body.data)
+    console.log(`Q1: ${req.body.data.question1} and Q2: ${req.body.data.question2}`)
+    // console.log(req.body.data.id)
 
-    Question.findOneAndUpdate({ id: req.body._id }, {
+    Question.findOneAndUpdate({ id: req.body.data._id }, {
 
         // data: req.body.data.question1,
         // data: req.body.question2,
         // question2: req.body.data.question2,
-        // question1: req.body.question1,
-        // question2: req.body.question2
+        question1: req.body.data.question1,
+        question2: req.body.data.question2
     })
-    // .then(() => {
-
-    // })
+    .then(() => {
+        res.sendStatus(200)
+    })
 })
 
 router.delete("/", (req, res) =>{
     console.log("Delete request sent");
-    console.log(req.body._id)
+    console.log(req.body)
 
     Question.findOneAndDelete({ id: req.body._id })
     .then(() => {
