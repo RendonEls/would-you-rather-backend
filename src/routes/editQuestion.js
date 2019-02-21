72480b9a3c38ccfecb9e21865f235980fdb1318b
Pierson Brannan <plbrannan95@gmail.com>
2019-02-21 11:37:33 -0500

    Draft towards update button

--- src/routes/editQuestion.js
+++ src/routes/editQuestion.js
@@ -8,17 +8,24 @@ router.get("/", (req, res) =>{
     Question.find().then(show => res.json(show))
     // res.send("This is a edit question page")
 })
+
 router.put("/", (req, res) =>{
     // res.send("This is PUT question route")
     console.log("PUT request sent")
     console.log(req.body._id)
-    Question.findOneAndUpdate({ id: req.body_id }, {
-        question1: req.body.question1
-    })
-    .then(() => {
 
+    Question.findOneAndUpdate({ id: req.body._id }, {
+        // data: req.body.data.question1,
+        // data: req.body.question2,
+        // question2: req.body.data.question2,
+        question1: req.body.question1,
+        question2: req.body.question2
     })
+    // .then(() => {
+
+    // })
 })
+
 router.delete("/", (req, res) =>{
     console.log("Delete request sent");
     console.log(req.body._id)
