c722f482541b28886ad976e608d62fb105c6c4b7
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-22 10:24:30 -0500

    moved cors placement

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const cors = require('cors')
 const express = require('express')
 const parser = require('body-parser')
-const cors = require('cors')
 const mongoose = require('./src/db/connection')
 const routes = require('./src/routes/index')
 
 
 const app = express()
+app.use(cors())
 
 app.set('port', process.env.PORT || 3001)
 app.use(parser.json())
-app.use(cors())
 
 app.use('/', routes)
 
