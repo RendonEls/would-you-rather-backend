b8bf9af606ef7c0898229d65039fec6b3c4a7494
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-22 10:41:38 -0500

    minor edit to connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})  //for deployed 
 } else {
-    mongoose.connect("mongodb://localhost/3001", {useMongoClient: true});
+    mongoose.connect("mongodb://localhost:3001", {useMongoClient: true});
 }
 
 mongoose.Promise = Promise
